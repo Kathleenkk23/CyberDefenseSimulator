113e39dc06f86cae7fcb2775bcd4a68973793491
Kathleenkk23 <89649648+Kathleenkk23@users.noreply.github.com>
2023-02-02 00:18:16 -0600

    set up

--- CyberDefenseSimulator.py
+++ CyberDefenseSimulator.py
@@ -1,44 +1,115 @@
-class CyberDefenseSimulator:
-    class OperatingSystem:
-        def init(self, id, type, verson, vulnerability):
-            self.id = id
-            self.type = type
-            self.verson = verson
-            self.vulnerability = vulnerability
-        
-        def getId(self):
-            return self.id
-        
-        def getType(self):
-            return self.type
-        
-    
-    class App:
-        def init(self, id, type, verson, vulnerability):
-            self.id = id
-            self.type = type
-            self.verson = verson
-            self.vulnerability = vulnerability
-            
-        def getId(self):
-            return self.id
-        
-        def getType(self):
-            return self.type
-        
-            
-    class Device:
-        def init(self, OS, apps, address):
-            self.OS = OS
-            self.apps = apps
-            self.address = address
-        
-        def getAddress(self)
-            return self.address
-            
-    
-    
+from enum import Enum
+#class CyberDefenseSimulator:
 
-Subnet = {}
-   
+# App: Id, type, vulneralbility, version
+# OS: ID, type, version, vulnerabilities
+# Device: OS, {app}, address
+# Subnet: set of devices
+# network: set of subnet
+# explicit: vulnerability, OS, app
+# workflow: source, test, size(# of steps)
+# OS: Id, type, version, vulnerabilities
+# Vulnerability
+
+#class CyberDefenseSimulator
+class CyberDefenseSimulator:
+    def init(self):
+        # Subnet: set of devices
+        self.subnet = {}
+        # network: set of subnet
+        self.network = {}
+    
+    def getinfo(self):
+        print("subnet : " + self.subnet)
+        print("OS type: " + self.network)
+
+
+# OS: ID, type, version, vulnerabilities
+class OperatingSystem:
+    def init(self, id, type, verson):
+        self.id = id
+        self.type = type
+        self.verson = verson
+        self.vulnerabilities = Vulnerability.noVulneralbility
+
+    def getId(self):
+        print("OS id: " + self.id)
+        return self.id
+
+    def getType(self):
+        print("OS type: " + self.type)
+        return self.type
+
+    def getVersion(self):
+        print("OS version: " + self.verion)
+        return self.version
+    
+    def setVulnerabilities(self, num):
+        self.vulneralbilities = Vulnerability(num)
+        
+    def getinfo(self):
+        print("OS id: " + self.id)
+        print("OS type: " + self.type)
+        print("OS version: " + self.verion)
+
+
+# App: Id, type, vulneralbility, version
+class App:
+    def init(self, id, type, verson):
+        self.id = id
+        self.type = type
+        self.verson = verson
+        self.vulnerability = Vulnerability.noVulneralbility
+
+    def getId(self):
+        return self.id
+
+    def getType(self):
+        return self.type
+    
+    def getVersion(self):
+        return self.version
+    
+    def setVulnerabilities(self, num):
+        self.vulneralbilities = Vulnerability(num)
+        
+    def getinfo(self):
+        print("app id: " + self.id)
+        print("app type: " + self.type)
+        print("app version: " + self.verion)
+
+# Device: OS, {app}, address
+class Device:
+    def init(self, OS, address):
+        self.OS = OS #operatingSystem
+        self.apps = {}
+        self.address = address
+
+    def getAddress(self):
+        return self.address
+    
+    def addApps(self, appName):
+        if isinstance(appName, App):
+            self.apps.add(appName)
+            print("app added successfully")
+        else:
+            print("not an app")
             
+    def getinfo(self):
+        print("device address: " + self.address)
+        print("OS type: " + self.OS.type)
+        print("OS version: " + self.OS.verion)
+
+
+class Vulnerability(Enum):
+    noVulneralbility = 0
+    vultnerability1 = 1
+    vultnerability2 = 2
+    vultnerability3 = 3
+
+
+# Subnet: set of devices
+subnet = {}
+
+# network: set of subnet
+network = {}
